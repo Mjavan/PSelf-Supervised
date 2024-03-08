5293b345fc2ea222cc67f9050c029fc96da910ec
Mjavan <48623760+Mjavan@users.noreply.github.com>
2024-03-08 22:35:00 +0100

    Update bayesianbyol.py

--- core/bayesianbyol.py
+++ core/bayesianbyol.py
@@ -1,4 +1,4 @@
-# /core/Methods/bayesianbyol.py
+# /core/bayesianbyol.py
 
 import sys
 sys.path.insert(0, "../core")
@@ -28,19 +28,15 @@ import datetime
 from pathlib import Path 
 
 
-from ..networks import networkbyol
-from ..Dataloader.dataloader import get_transform, pair_aug, get_train_val_loader
-from ..Optimizer.csghmc import SGHM
-from ..Optimizer.lr_scheduler import update_lr, update_lin 
-
-
+from networks import networkbyol
+from dataloader import get_transform, pair_aug, get_train_val_loader
+from csghmc import SGHM
+from lr_scheduler import update_lr, update_lin 
 
 import warnings 
 warnings.filterwarnings('ignore')
 
-
 #### Main training
-
 parser = argparse.ArgumentParser(description='BByol pretraining')
 
 parser.add_argument('--seed',type=int,default=42,
@@ -77,8 +73,6 @@ parser.add_argument('--base_target_ema',type=float, default=0.996,
 
 parser.add_argument('--temp',type=float, default=0.1, 
                         help = 'Temprature for cold posterior in sghm opt!')
-
-
 # data
 parser.add_argument('--in_size',type=int, default=32,
                        help='The input size of images')
@@ -149,8 +143,7 @@ def main(args):
     torch.backends.cudnn.benchmark = False
     random.seed(seed)
     np.random.seed(seed)
-    
-    
+  
     ## setting device 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     use_cuda = torch.cuda.is_available()
@@ -164,8 +157,7 @@ def main(args):
     
     ## saving version packages
     #os.system(f'conda env export > {save_dir}/yml/{args.optimizer}_{args.exp}_env.yml')
-
-    
+  
     ## saving hyperparameters
     HPS = vars(args)    
     with open(save_dir /'params'/'byol_params'/'pretrain'/args.ds/f'{args.exp}_{args.model_type}param.json','w') as file:    
@@ -192,8 +184,7 @@ def main(args):
     elif args.ds == 'tinyimagenet':
         path_tinyimagenet = save_dir/'data'/'tiny-imagenet-200'/'train'
         dataset = datasets.ImageFolder(path_tinyimagenet,transform=pair_aug(get_transform(args.in_size,args.ds,args.s)))
-        
-                
+                    
     train_loader,val_loader = get_train_val_loader(dataset,val_size=args.val_size,batch_size=args.batch_size,\
                                                    num_workers=args.num_workers,seed=args.seed)
     
@@ -203,7 +194,6 @@ def main(args):
     cycle_batch_length = args.cycle_length * n_batch
     batch_idx = 0
 
-    
     ## getting models 
     if args.model_depth=="res18":
         backbone = models.resnet18(pretrained=False, progress=True)
@@ -211,22 +201,17 @@ def main(args):
     elif args.model_depth=='res50':
         backbone = models.resnet50(pretrained=False, progress=True)
     
-    
     online_network = networkbyol('online',backbone,args.mlp_hid_size,args.proj_size).to(device)
     target_network = copy.deepcopy(networkbyol('target',backbone,args.mlp_hid_size,args.proj_size)).to(device)
     
-    
     ## getting optimizer
     if args.optimizer=='adam':
-        
         optimizer = optim.Adam(online_network.parameters(),args.lr,weight_decay=args.wd)
         
     elif args.optimizer=='sgd':
-        
         optimizer = optim.SGD(online_network.parameters(),lr=args.lr,weight_decay=args.wd,momentum=0.9)
         
     elif args.optimizer=='sghm':
-        
         optimizer = SGHM(params=online_network.parameters(),lr=args.lr,weight_decay=args.wd/N_train,momentum=0.9,\
                          temp=args.temp,addnoise=1,dampening=0.0,N_train=N_train)    
         
@@ -243,10 +228,7 @@ def main(args):
     
     print(f'training is started')
     for epoch in range(args.num_epochs):
-        
-        # time for training and val
         tic = time.time()
-        
         for phase in ['train','val']:
             
             if phase=='train':
@@ -271,12 +253,10 @@ def main(args):
             for (img1,img2),_ in dataloader:
                 
                 img1= img1.to(device)
-            
                 img2= img2.to(device)
                 
                 loss = Train(online_network,target_network,optimizer,img1,img2,phase,\
-                             N_train,batch_idx,cycle_batch_length,epoch)
-                             
+                             N_train,batch_idx,cycle_batch_length,epoch)             
                 total_loss += loss.item()
                 
                 if phase=='train':    
@@ -293,42 +273,34 @@ def main(args):
                         online_network.cpu()
                     torch.save(online_network.state_dict(),os.path.join(save_dir_mcmc,f'model_{mt}.pt'))
                     mt +=1
-                    online_network.cuda()
-                            
+                    online_network.cuda()        
                     print(f'sample {mt} from {args.N_samples} was taken!')
                     print(f'sampled epoch lr:%.7f'%(optimizer.param_groups[0]['lr']))
-            
-        
+                  
         toc = time.time()
         runtime_epoch = toc - tic
         lr_epoch = optimizer.param_groups[0]['lr']
-        
         print('Epoch: %d Train_Loss: %0.4f, Val_Loss: %0.4f, time:%0.4f seconds, lr:%0.7f'%(epoch,history['train'][epoch],\
                                                                                   history['val'][epoch],
-                                                                                  runtime_epoch,lr_epoch))
-                                                                          
+                                                                                  runtime_epoch,lr_epoch))                                                                  
         ### save best model    
         if history['val'][epoch] < best_val:
-            
             best_val = history['val'][epoch]
             check_without_progress = 0 
             torch.save({'epoch':epoch+1,
                         'model':online_network.state_dict(),
                         'optimizer':optimizer.state_dict(),
                         'Loss':history['val'][epoch]},os.path.join(save_dir_epoch,f'{args.exp}_best_{args.model_type}byol.pt'))
-               
+          
     ### save last model
     torch.save({'epoch':epoch+1,
                 'model':online_network.state_dict(),
                 'optimizer':optimizer.state_dict(),
                 'Loss':history['val'][epoch]},os.path.join(save_dir_epoch,f'{args.exp}_last_{args.model_type}byol.pt'))
     print(f'save last model')
-                                          
-            
+                                                
 def loss_fn(online_network,target_network,img1,img2,phase):
-    
     with torch.set_grad_enabled(phase == 'train'):
-        
         onl_pred1 = online_network(img1)
         onl_pred2 = online_network(img2)
     
@@ -343,71 +315,45 @@ def loss_fn(online_network,target_network,img1,img2,phase):
     
         loss = reg_loss(onl_pred1,tar_proj2)
         loss += reg_loss(onl_pred2,tar_proj1)
-    
         return(loss.mean())
-        
-
-            
-def Train(online_network,target_network,optimizer,img1,img2,phase,N_train,batch_idx,cycle_batch_length,epoch):
-          
+                   
+def Train(online_network,target_network,optimizer,img1,img2,phase,N_train,batch_idx,cycle_batch_length,epoch):     
     loss = loss_fn(online_network,target_network,img1,img2,phase)
-    
-    if phase=='train':
-            
+    if phase=='train':    
         # Update online network
         if args.scale:
-            loss = loss * N_train
-            
+            loss = loss * N_train    
         optimizer.zero_grad()
-        
         # Update lr 
         if args.lr_sch == 'cyc':
-            
             update_lr(args.lr,batch_idx,cycle_batch_length,args.n_sam_cycle,optimizer)
-            
         if args.lr_sch == 'lin':
-            
             lr = args.lr*np.exp(-args.lr_dec*min(1.0,(batch_idx*args.batch_size)/float(N_train)))
-
             update_lin(lr,optimizer)
-        
         # Inject noise to parameter update
         if args.lr_sch == 'cyc' and args.epoch_noise:
-            
             if (epoch%args.cycle_length)+1 > args.epoch_noise:
-                
                 optimizer.param_groups[0]['epoch_noise'] = True                       
             else:         
-                optimizer.param_groups[0]['epoch_noise'] = False
-                
-                
-        elif args.lr_sch in ['lin','fixed'] and args.epoch_noise:
-            
+                optimizer.param_groups[0]['epoch_noise'] = False        
+        elif args.lr_sch in ['lin','fixed'] and args.epoch_noise: 
             if epoch >= args.epoch_noise:
-                
                 optimizer.param_groups[0]['epoch_noise'] = True 
-                
             else:
-                optimizer.param_groups[0]['epoch_noise'] = False
-            
+                optimizer.param_groups[0]['epoch_noise'] = False    
         loss.backward() 
         optimizer.step()
-        
+      
         # Update target network
         tau = args.base_target_ema
-    
         for online_params,target_params in zip(online_network.parameters(),target_network.parameters()):
             target_params.data = tau*target_params +(1-tau)*online_params
-        
         # Default is False
         if args.scale:
-            loss = loss/N_train
-            
+            loss = loss/N_train        
     return(loss)
-
-
+  
 #### Running the code
-if __name__=='__main__':
-    
+if __name__=='__main__':  
     main(args)
     

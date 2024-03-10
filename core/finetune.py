4b59e9801905a6c6d641c557dab5b33fcab22e05
Mjavan <48623760+Mjavan@users.noreply.github.com>
2024-03-10 22:02:03 +0100

    Update finetune.py

--- core/finetune.py
+++ core/finetune.py
@@ -28,6 +28,7 @@ from pathlib import Path
 from networks import networkbyol,networksimclr,finetune_net
 from utils import evaluation_metrics
 from dataloader import get_transform_ft as get_transform
+from single_finetune import single_finetune
 
 try:
     import cPickle as pickle

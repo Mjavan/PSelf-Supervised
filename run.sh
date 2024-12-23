28e8dc566ba51a0295a676c9b5276d4011cfd377
masoumeh javanbakhat <masoumeh.javanbakhat@hpc.dhclab.i.hpi.de>
2024-12-23 15:37:45 +0100

    Adding run.sh file

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Default values for arguments
+DEFAULT_SCRIPT="core/BayesianByol.py"
+seed=42
+exp=600
+num_epochs=20
+
+
+
+# Check if arguments are passed, otherwise use defaults
+SCRIPT=${1:-$DEFAULT_SCRIPT}
+ARG1=${2:-$seed}
+ARG2=${3:-$exp}
+ARG3=${4:-$num_epochs}
+
+
+
+# Run the Python script with the arguments
+python $SCRIPT --seed $ARG1 --exp $ARG2 --num_epochs $ARG3 
